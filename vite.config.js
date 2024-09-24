cdbbb63b87af452742f4908247f840baaf83b307
BotThice <thiranut.120346@gmail.com>
2024-09-24 08:32:31 +0700

    Add nginx as loadbalancer service to balance load of laravel.test

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',  // Listen on all interfaces (for Docker compatibility)
+        hmr: {
+            host: 'localhost',  // You may need to adjust this based on how you're accessing the app (could be your actual host URL)
+        },
+    },
 });
